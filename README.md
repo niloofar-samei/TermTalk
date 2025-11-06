8105e226681fc48e05b592bb1c0be8301ab9ecd5
Pouya Abbassi <abbassi.pouya@gmail.com>
2025-11-07 03:26:52 +0330

    add postgres
    fixup
    
    fixup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TermTalk 
 A chat project using Node.js + TypeScript + Express for backend and React + TypeScript + Vite for frontend.
+
+# To run DB
+In the root of the project, run:
+```
+postgres
+```
--- flake.nix
+++ flake.nix
@@ -31,7 +31,43 @@
             react-static
             python313Packages.prisma
             prisma
+            postgresql
+            postgresql.pg_config
           ]);
+          shellHook = ''
+            # init the db with
+            mkdir -p ./db/ || echo
+            export PGDATA="$(pwd)/db"
+            export PGHOST="$(pwd)"
+            export PGPORT="5432"
+
+            if [[ ! $(grep listen_addresses $PGDATA/postgresql.conf) ]]; then
+              echo "db does not exist, creating "
+              initdb -D $PGDATA --no-locale --encoding=UTF8
+
+              echo "listen_addresses = 'localhost'" >> $PGDATA/postgresql.conf
+              echo "port = $PGPORT" >> $PGDATA/postgresql.conf
+              echo "unix_socket_directories = '$PGHOST'" >> $PGDATA/postgresql.conf
+
+              # ...create a database using the name Postgres defaults to.
+              echo "CREATE USER postgres SUPERUSER;" | postgres --single -E postgres
+              echo "CREATE DATABASE postgres WITH OWNER postgres;" | postgres --single -E postgres
+              # ...create a user and database for the project.
+              echo "CREATE USER dbuser WITH PASSWORD '11223344';" | postgres --single -E postgres
+              echo "CREATE DATABASE termtalk WITH OWNER dbuser" | postgres --single -E postgres
+              echo "GRANT ALL PRIVILEGES ON SCHEMA public TO dbuser" | postgres --single -E postgres
+            fi
+
+            # postgres &
+
+            ## command to access the db after start
+            # psql -h localhost termtalk # or postgres
+
+            ## command to kill the db
+            # pg_ctl -D ./db stop
+
+            # trap "pg_ctl -D ./db stop" EXIT
+          '';
         };
       };
       flake = {

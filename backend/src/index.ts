58210b57d7a7bfefbce9db43b96c11912dfd9859
niloofar-samei <niloofar.samei@gmail.com>
2025-11-09 23:27:54 +0000

    Remove duplicate cors.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,16 +14,13 @@ const app = express();
 const server = http.createServer(app);
 const PORT = 4000;
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "http://localhost:5173",
   methods: ["GET", "POST"],
   credentials: true,
+  }
 });
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST"],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 io.on("connection", (socket) => {

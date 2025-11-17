7257bd1fab69447153218ff0da7d79d2392f6f1d
niloofar-samei <niloofar.samei@gmail.com>
2025-11-17 22:06:10 +0000

    Linting and cleanup.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,21 +20,11 @@ const app = express();
 const server = http.createServer(app);
 const PORT = 4000;
 
-// Initialize socket.io and allow my React frontend to connect.
-// Attach socket.io to the HTTP server so it can handle
-// real-time WebSocket connections.
-
-// cors: {...} sets cors rules which control which websites
-// can talk to my backend.s
-
-// methods specifies which HTTP methods are allowed for
-// CORS preflight requests. Socket.io sometimes uses HTTP
-// requests first to establish the connection before
-// switching to WebSocket.
+// Initialize socket.io and allow my React frontend to connect. Attach socket.io to the HTTP server so it can handle real-time WebSocket connections.
+// cors: {...} sets cors rules which control which websites can talk to my backend.
+// methods specifies which HTTP methods are allowed for CORS preflight requests. Socket.io sometimes uses HTTP requests first to establish the connection before switching to WebSocket.
 // It only applies to HTTP requests (REST API calls).
-
-// credentiols allows the browser to send cookies or
-// authentication headers with the request.
+// credentiols allows the browser to send cookies or authentication headers with the request.
 const io = new Server(server, {
   cors: { origin: "http://localhost:5173",
   methods: ["GET", "POST"],
@@ -42,24 +32,21 @@ const io = new Server(server, {
   }
 });
 
-// Middlewares
-// Enable cross-origin requests. It only applies to WebSocket
-// connections.
+//---- Middlewares ----///
+
+// Enable cross-origin requests. It only applies to WebSocket connections.
 app.use(cors());
 
-// Allows my server to automatically understand JSON data
-// from the frontend.
+// Allows my server to automatically understand JSON data from the frontend.
 app.use(express.json());
 
 const JWT_SECRET = "asus";
 
-
-
 let onlineUsers = 0;
 
-// Register
+// Register endpoint
 app.post("/register", async (req, res) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
   const hashed = await bcrypt.hash(password, 10);
 
   try{
@@ -67,29 +54,24 @@ app.post("/register", async (req, res) => {
     res.json({user: result.rows[0]});
   } catch (err) {
     res.status(400).json({error: "Could not insert into db."});
-    //console.error("Register error: ", err);
-    //res.status(500).json({error: "Registration failed."});
   }
 });
 
-// Login
-
+// Login endpoint
 app.post("/login", async (req, res) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
   const result = await pool.query("SELECT * FROM users WHERE username=$1", [username]);
 
-  if (result.rows.length === 0) return res.status(401).json({error:"Invalid username or password"});
+  if (result.rows.length === 0) return res.status(401).json({ error:"Invalid username or password" });
 
   const user = result.rows[0];
   const match = await bcrypt.compare(password, user.password_hash);
 
-  if (!match) return res.status(401).json({error: "Invalid username or password"});
+  if (!match) return res.status(401).json({ error: "Invalid username or password" });
 
-  const token = jwt.sign({id: user.id, username: user.username}, JWT_SECRET, {expiresIn: "1d"});
+  const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1d" });
   res.json({ token });
 })
-
-
 
 // Runs everytime a user connects
 io.on("connection", (socket) => {
@@ -99,26 +81,20 @@ io.on("connection", (socket) => {
   io.emit("online users", onlineUsers);
 
 // Receive message from client without timestamp
-
 // async allows us to use await inside for db operations.
-
-// (msg: Omit<ChatMessage, "timestamp">) means msg has all
-// properties of ChatMessage except timestamp.
+// (msg: Omit<ChatMessage, "timestamp">) means msg has all properties of ChatMessage except timestamp.
 socket.on("chat message", async (msg: Omit<ChatMessage, "timestamp">) => {
   const timestamp = new Date().toLocaleTimeString();
 
   // Combines user message + server timestamp
-  
-  // { ...msg, timestamp } uses the spread operator (...) to
-  // copy all fields from msg.
+  // { ...msg, timestamp } uses the spread operator (...) to copy all fields from msg.
   const messageWithTime: ChatMessage = { ...msg, timestamp };
 
   console.log("💬 Received message:", messageWithTime);
 
   // Save to PostgreSQL
   try {
-    // Here await tells JS to wait for this Promise to finish
-    // before going to the next line.
+    // Here await tells JS to wait for this Promise to finish before going to the next line.
     const result = await pool.query(
       "INSERT INTO chat_messages(username, text, timestamp) VALUES($1, $2, $3)",
       [msg.username, msg.text, timestamp]
@@ -140,12 +116,10 @@ socket.on("chat message", async (msg: Omit<ChatMessage, "timestamp">) => {
   });
 });
 
-// Creates a GET API endpoint for frontend to fetch all
-// messages from database. Sends them in JSON format.
+// Creates a GET API endpoint for frontend to fetch all messages from database. Sends them in JSON format.
 app.get("/messages", async (req, res) => {
   try {
-    // Here await tells JS to wait for this Promise to finish
-    // before going to the next line.
+    // Here await tells JS to wait for this Promise to finish before going to the next line.
     const result = await pool.query(
       "SELECT username, text, timestamp FROM chat_messages ORDER BY id ASC"
     );
@@ -162,33 +136,3 @@ app.get("/messages", async (req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-
-// login o register beran tu file e jodaa
-// check kone username already exist
-// jwt ro bezaaram tu .env
-// code e frontend
-// {auea} => { oue } va comment haa 1 khat beshan
-// password, 10 mishe adda esho avaz kard?
-
-//If you want, I can now:
-// add JWT verification middleware
-// show how to protect a route /profile
-// send the token through socket.io
-
-// chat shoul not ask username manually
-//show chat ui only when logged-in
-
-//You do NOT verify the token
-
-//You do NOT protect any routes
-
-//You do NOT decode the token
-
-//You cannot attach the logged-in user to messages
-
-//You cannot check if the user is allowed to send messages
-
-//You do NOT store the token in the frontend
-
-//You do NOT send the token with requests

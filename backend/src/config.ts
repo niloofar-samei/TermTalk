54a959f00f1b4fee2fbb2125a4a7b8439d8544a7
niloofar-samei <niloofar.samei@gmail.com>
2025-11-23 23:33:46 +0000

    Refactor: use safer JWT_SECRET config with validation in runtume check.

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,11 @@
 import dotenv from "dotenv";
+// Load environment variables from .env file
 dotenv.config();
 
-export const JWT_SECRET = process.env.JWT_SECRET!;
+// Ensure JWT_SECRET is set
+if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is missing in .env, please set it!");
+}
+
+// Export all global config values
+export const JWT_SECRET = process.env.JWT_SECRET;
